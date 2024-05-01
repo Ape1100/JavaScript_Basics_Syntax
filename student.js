3ba8d51989db621b9f893142a7a9387d221ff6cb
Edwin McMillan <edwinmcmillan7@gmail.com>
2024-05-01 13:19:59 -0300

    Javascript_syntax

---
+++ student.js
@@ -0,0 +1,39 @@
+// Task 1 Student information
+
+let studentName;
+let studentAge;
+let studentGrade;
+let isPresent;
+
+// Task 2 Smaple values 
+studentName = "Edwin McMillan";
+studentAge = 21;
+studentGrade = 12;
+isPresent = true;
+
+// Values
+let num1;
+let num2;
+
+// Sample values
+num1 = 10;
+num2 = 5;
+
+// Task 3 Display student information 
+console.log("Sum:", num1 + num2);
+console.log("Difference:", num1 - num2);
+console.log("Product:", num1 * num2);
+console.log("Quotient:", num1 / num2);
+
+
+num1 += 2;
+num2 -= 1;
+console.log("Updated num1:", num1);
+console.log("Updated num2:", num2);
+
+
+console.log("Is num1 equal to num2?", num1 === num2);
+console.log("Is num1 greater than num2?", num1 > num2);
+console.log("Is num1 not equal to num2?", num1 !== num2);
+
+
